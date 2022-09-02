0610ae1bb0c76bc9d7bcdbf0f8cd12e15348cbc7
Saurav Kumar <104059310+pandeyji9570@users.noreply.github.com>
2022-09-02 14:44:00 +0530

    Delete Parking_Slot Database.sql

--- Parking_Slot Database.sql
+++
@@ -1,5 +0,0 @@
-create database Parking_Slot;
-use Parking_Slot ;
-Create table user_details( First_Name varchar(20), Last_Name varchar(20), mobileNo bigint unique, Vehicle_Type varchar(20), Vehicle_No varchar(20));
-
-select *from user_details;
